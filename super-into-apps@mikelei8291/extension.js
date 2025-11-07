d7de08e9998395faf5551fdf81cded6a80b62303
Mike Lei <mikelei8291@users.noreply.github.com>
2025-11-08 01:12:10 +0800

    feat: remove all unrelated functionalities and rename the extension

--- super-into-apps@mikelei8291/extension.js
+++ super-into-apps@mikelei8291/extension.js
@@ -1,30 +1,25 @@
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as Overview from 'resource:///org/gnome/shell/ui/overview.js';
 
-export default class StartOverlayInAppViewExtension extends Extension {
+export default class SuperIntoAppsExtension extends Extension {
     originalToggle;
 
     constructor(metadata) {
         super(metadata);
-
         this.originalToggle = Overview.Overview.prototype.toggle;
     }
 
     enable() {
         Overview.Overview.prototype.toggle = function () {
-            if (this.isDummy)
+            if (this.isDummy) {
                 return;
-
-            if (this._visible)
+            }
+            if (this._visible) {
                 this.hide();
-            else
+            } else {
                 this.showApps();
+            }
         };
-        
-        if (Main.layoutManager._startingUp){
-            Main.layoutManager.connectObject('startup-complete', () => Main.overview.showApps(), this);
-        }
     }
 
     disable() {
